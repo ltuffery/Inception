3a95f8037f7dbd4dc75313240ff344e07e93853e
ltuffery <123221865+ltuffery@users.noreply.github.com>
2024-03-14 14:27:22 +0100

    fix wordpress

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -4,22 +4,20 @@ if [ ! -d /run/mysqld ]; then
     chown -R mysql:mysql /run/mysqld
 fi
 
-# {
-#     echo "FLUSH PRIVILEGES;"
-#     echo "CREATE DATABASE IF NOT EXISTS \`wordpress\`;"
-#     echo "CREATE USER \`caca\`@'%' IDENTIFIED BY 'pass';"
-#     echo "GRANT ALL PRIVILEGES ON \`wordpress\`.* TO \`caca\`@'%' IDENTIFIED BY 'pass';"
-#     echo "FLUSH PRIVILEGES;"
-# } | mysqld --bootstrap --user=mysql --datadir=/var/lib/mysql
+sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf
+sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
+
+sed -i "s|.*skip-networking.*|skip-networking=OFF|g" /etc/my.cnf
+sed -i "s|.*skip-networking.*|skip-networking=OFF|g" /etc/my.cnf.d/mariadb-server.cnf
 
 mysqld --bootstrap --user=mysql --datadir=/var/lib/mysql <<-EOF
 FLUSH PRIVILEGES;
 CREATE DATABASE IF NOT EXISTS $DB_DB;
-CREATE USER $DB_USER@'%' IDENTIFIED BY '$DB_PASS';
+CREATE USER IF NOT EXISTS $DB_USER@'%' IDENTIFIED BY '$DB_PASS';
 GRANT ALL PRIVILEGES ON $DB_DB.* TO $DB_USER@'%' IDENTIFIED BY '$DB_PASS';
 FLUSH PRIVILEGES;
 EOF
 
 chown -R mysql:mysql /var/lib/mysql
 
-mysqld -umysql
+mysqld -umysql --port=3306
--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -1,7 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
-wp core download --allow-root
-wp config create --dbname="$DB_DB" --dbuser="$DB_USER" --dbpass="$DB_PASS" --dbhost="$DB_HOST":"$DB_PORT" --dbprefix='wp' --allow-root
+if [ ! -f "index.php" ]; then
+    wp core download --allow-root
+fi
+#sleep 5
+
+if [ ! -f "wp-config.php" ]; then
+    wp config create --dbname="$DB_DB" --dbuser="$DB_USER" --dbpass="$DB_PASS" --dbhost="$DB_HOST":"$DB_PORT" --allow-root
+fi
+
 wp core install --url="$WP_ADMIN_USER.42.fr" --title="$WP_ADMIN_USER inception website" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASS" --admin_email="$WP_ADMIN_USER@student.42angouleme.fr" --allow-root
 wp user create $WP_DEFAULT_USER "$WP_DEFAULT_USER"@randomuser.com --role='subscriber' --user_pass="$WP_DEFAULT_PASS" --allow-root
 wp option update comment_registration 1 --allow-root
