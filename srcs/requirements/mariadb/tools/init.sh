45c78e61262344a64340a15298a4bb6717e254eb
ltuffery <123221865+ltuffery@users.noreply.github.com>
2024-03-14 11:27:29 +0100

    fix create db user

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -4,12 +4,22 @@ if [ ! -d /run/mysqld ]; then
     chown -R mysql:mysql /run/mysqld
 fi
 
-{
-    echo "FLUSH PRIVILEGES;"
-    echo "CREATE DATABASE IF NOT EXISTS \`wordpress\`;"
-    echo "CREATE USER \`caca\`@'%' IDENTIFIED BY 'pass';"
-    echo "GRANT ALL PRIVILEGES ON \`wordpress\`.* TO \`caca\`@'%' IDENTIFIED BY 'pass';"
-    echo "FLUSH PRIVILEGES;"
-} | mysqld --bootstrap --user=mysql --datadir=/var/lib/mysql
+# {
+#     echo "FLUSH PRIVILEGES;"
+#     echo "CREATE DATABASE IF NOT EXISTS \`wordpress\`;"
+#     echo "CREATE USER \`caca\`@'%' IDENTIFIED BY 'pass';"
+#     echo "GRANT ALL PRIVILEGES ON \`wordpress\`.* TO \`caca\`@'%' IDENTIFIED BY 'pass';"
+#     echo "FLUSH PRIVILEGES;"
+# } | mysqld --bootstrap --user=mysql --datadir=/var/lib/mysql
+
+mysqld --bootstrap --user=mysql --datadir=/var/lib/mysql <<-EOF
+FLUSH PRIVILEGES;
+CREATE DATABASE IF NOT EXISTS $DB_DB;
+CREATE USER $DB_USER@'%' IDENTIFIED BY '$DB_PASS';
+GRANT ALL PRIVILEGES ON $DB_DB.* TO $DB_USER@'%' IDENTIFIED BY '$DB_PASS';
+FLUSH PRIVILEGES;
+EOF
 
 chown -R mysql:mysql /var/lib/mysql
+
+mysqld -umysql
